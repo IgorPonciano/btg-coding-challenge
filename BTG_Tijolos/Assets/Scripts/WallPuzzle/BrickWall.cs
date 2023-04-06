57354d30ee0d93cc3004396007242e0e19ab6f36
IgorPonciano <igorponciano.work@gmail.com>
2023-04-06 13:02:06 -0300

    Updates visuais, "UX" e fix randoms
    - Fixed a bug where random.Next would stop generating unique values after a couple of repetitions.
    - Changed sample scene canvas layout and ajusted the anchors.
    - Added the option to chose between rendering the wall or just making the calculations (gigantic walls don't work visually well for rendering in the scene (values up to 90x90 are ok).
    - Made drawWall method an IEnumerator to avoid freezing the machine if the number of loops was too high.
    - Added visual feedback for the time when the wall drawing is being loaded, so the user can know the app didn't bug or freeze the drawing.

--- BTG_Tijolos/Assets/Scripts/WallPuzzle/BrickWall.cs
+++ BTG_Tijolos/Assets/Scripts/WallPuzzle/BrickWall.cs
@@ -2,11 +2,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using TMPro;
+using System.Collections;
 
 public class BrickWall : MonoBehaviour
 {
     // * Unity does not serialize a List of Lists, so we can't setup this from the inspector *
-    public List<List<int>> wall = new List<List<int>>{
+    private List<List<int>> wall = new List<List<int>>{
         new List<int> {1,2,2,1},
         new List<int>{3,1,2},
         new List<int>{1,3,2},
@@ -18,104 +19,138 @@ public class BrickWall : MonoBehaviour
     [Header("UI Setup")]
     [SerializeField] private TextMeshProUGUI wallDrawText;
     [SerializeField] private TextMeshProUGUI puzzleAnwerText;
+    [SerializeField] private TextMeshProUGUI puzzleDescriptionText;
+
+    [Space(15)]
+    [SerializeField] private int wallHeightMax = 1000;
+    [SerializeField] private int wallWidthMax = 6;
+
+    [Space(15)]
+    [Tooltip("Use with small values for wallHeight and Width")]
+    [SerializeField] private bool drawWall = false;
+    [SerializeField] private GameObject loadingIcon;
 
     private void Start()
     {
         ApproachNewWall();
     }
 
-    public string DrawWall(List<List<int>> wall)
+    public IEnumerator DrawWall(List<List<int>> wall, TextMeshProUGUI wallText)
     {
-        string wallText = "";
+        loadingIcon.SetActive(true);
+        string newWallText = "";
         // Draw rows
         foreach (List<int> row in wall)
         {
             // print * for each tile width unit. In the last one add "|" and the start a new tile. At the end of the row, jump a line
             for (int tile = 0; tile < row.Count; tile++)
             {
-                wallText += "|";
+                newWallText += "|";
 
                 for (int i = 1; i <= row[tile]; i++)
                 {
-                    wallText += "*";
+                    newWallText += "*";
 
                     if (i != row[tile])
                     {
-                        wallText += " ";
+                        newWallText += " ";
                     }
+                    yield return null;
                 }
 
             }
-            wallText += "|";
-            wallText += "\n";
+            newWallText += "|";
+            newWallText += "\n";
         }
-        return wallText;
+        loadingIcon.SetActive(false);
+        wallText.text = newWallText;
     }
 
-    // Create a new random wall, following the restrictions -> WIP
-    public List<List<int>> GenerateNewWall(int wallHeightMax, int wallWidth)
+    // Create a new random wall, following the restrictions
+    public List<List<int>> GenerateNewWall(int wallHeight, int wallWidth)
     {
         List<List<int>> newWall = new List<List<int>>();
 
-        for(int i = 0; i < Random.Range(3, wallHeightMax+1); i++)
+        System.Random newRandom = new System.Random();
+
+        for (int i = 0; i < wallHeight; i++)
         {
             List<int> row = new List<int>();
             newWall.Add(row);
         }
 
-       foreach (List<int> row in newWall)
-       {
-        int availableSlots = wallWidth;
-        
-        do
+        foreach (List<int> row in newWall)
         {
-            int tile = Random.Range(1, availableSlots + 1);
-            availableSlots -= tile;
-            row.Add(tile);
-        } while (availableSlots > 0);
-       }
-       
+            int availableSlots = wallWidth;
+
+            do
+            {
+                // define tile width
+                int tile = newRandom.Next(1, availableSlots + 1);
+                availableSlots -= tile;
+                row.Add(tile);
+            } while (availableSlots > 0);
+        }
+
         return newWall;
     }
-    
-public int GetMinCuts(List<List<int>> wall)
-{
-    if (wall == null || wall.Count == 0) // handles not valid input values
-    {
-        return 0;
-    }
-    // Dictionary to store gaps between bricks in a row. [position, numberOfGaps]
-    Dictionary<int, int> dictOfGaps = new Dictionary<int, int>(wall.Count);
 
-    foreach (List<int> row in wall)
+    public int GetMinCuts(List<List<int>> wall)
     {
-        int pGap = 0;
-
-        // After each tile, count 1 gap at that position. Don't count for the last, because it would be the right border
-        for (int tile = 0; tile < row.Count - 1; tile++)
+        if (wall == null || wall.Count == 0) // handles not valid input values
         {
-            // Example: 2 | 2 | 2 -> gaps are in p2 and p4
-            pGap += row[tile];
+            return 0;
+        }
+        // Dictionary to store gaps between bricks in a row. [position, numberOfGaps]
+        Dictionary<int, int> dictOfGaps = new Dictionary<int, int>(wall.Count);
 
-            try
-            {
-                dictOfGaps[pGap] += 1;
-            }
-            catch (KeyNotFoundException ex)
-            {
-                dictOfGaps.Add(pGap, 1);
-            }
+        foreach (List<int> row in wall)
+        {
+            int pGap = 0;
 
+            // After each tile, count 1 gap at that position. Don't count for the last, because it would be the right border
+            for (int tile = 0; tile < row.Count - 1; tile++)
+            {
+                // Example: 2 | 2 | 2 -> gaps are in p2 and p4
+                pGap += row[tile];
+
+                try
+                {
+                    dictOfGaps[pGap] += 1;
+                }
+                catch (KeyNotFoundException ex)
+                {
+                    dictOfGaps.Add(pGap, 1);
+                }
+
+            }
+        }
+
+        // Avoid case where there is no elements in the dict of gaps
+        if (dictOfGaps.Count == 0)
+        {
+            return wall.Count;
+        }
+        // min number of bricks cut = number of rows - max gaps used * Avoid case where there is no elements in the dict
+        return wall.Count - dictOfGaps.OrderByDescending(p => p.Value).First().Value;
+    }
+
+    public void ApproachNewWall()
+    {
+        StopAllCoroutines();
+
+        System.Random random = new System.Random();
+        int wallWidth = random.Next(6, wallWidthMax + 1);
+        int wallHeight = random.Next(3, wallHeightMax + 1);
+
+        List<List<int>> randomWall = GenerateNewWall(wallHeight, wallWidth);
+        puzzleDescriptionText.text = ($"Wall is {wallHeight} meters tall and {wallWidth} meters wide");
+
+        puzzleAnwerText.text = ($"The minimum number of bricks cut is <color=yellow>{GetMinCuts(randomWall)}");
+
+        if (drawWall)
+        {
+            StartCoroutine(DrawWall(randomWall, wallDrawText));
         }
     }
-    // min number of bricks cut = number of rows - max gaps used
-    return wall.Count - dictOfGaps.OrderByDescending(p => p.Value).First().Value;
-}
-
-public void ApproachNewWall()
-{
-    List<List<int>> randomWall = GenerateNewWall(10, 6);
-    wallDrawText.text = DrawWall(randomWall);
-    puzzleAnwerText.text = ($"The minimum number of bricks cut is <color=yellow>{GetMinCuts(randomWall)}");
-}
 }
